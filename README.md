f0455849b8a94e3b62e7eabe30bfddfbc822c85b
Ana Bollain <ana.bollain@gmail.com>
2023-01-23 15:23:26 +0100

    Upload project files

---
+++ README.md
@@ -0,0 +1,21 @@
+# JS HTML5 Canvas
+
+## Description
+
+The aim of this project is to work with HTML5 `<canvas>` element and JS context to draw graphics on a web page. 
+
+In this particular example, you can draw multicolor lines that will create a psychedelic effect when overlapping by using the `difference` operation.
+
+## Javascript Used
+
+* getContext()
+* window and its properties
+* canvas properties such as lineJoin, lineCap, lineWidth, beginPath, etc.
+* flag variables 
+* mouse events and its properties (offsetX, offsetY)
+* destructuring syntax
+
+## Credits
+
+* [30 Day Vanilla JS Coding Challenge](https://javascript30.com/)
+
---
+++ main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+//Selectors
+const canvas = document.querySelector('#draw');
+//We don't draw directly on the canvas in HTML5, but in the context
+const ctx = canvas.getContext('2d');
+//By default, the size of the canvas will be 800x800px
+//To adjust it to the window size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+//Starting color
+ctx.strokeStyle = '#BADA55';
+//Lots of options to select from
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+ctx.globalCompositeOperation = 'difference';
+
+//Flag to check if the cursor is down when moving it
+let isDrawing = false;
+//Start and ending axes
+let lastX = 0;
+let lastY = 0;
+//Variable to change color
+let hue = 0;
+//Variable to change line width
+let direction = true;
+
+//Functions
+function draw(e){
+    if (!isDrawing) return; //stop the function from running when they are not moused down
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    //start from
+    ctx.moveTo(lastX, lastY);
+    //go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    //Update values to last position
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+    if(hue >= 360){
+        hue = 0;
+    }
+    //Set max and min width and use a flag to increment or decrease the width
+    if(ctx.lineWidth >= 200 || ctx.lineWidth <= 30){
+        direction = !direction;
+    }
+    if(direction){
+        //From 0 to 100 increment
+        ctx.lineWidth++;
+    }else{
+        //Once we reach the top or bottom, change logic
+        ctx.lineWidth--;
+    }
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    //To avoid lines always starting from the last position, each time we draw we reset them
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
